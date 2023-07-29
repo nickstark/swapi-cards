e6c3a8c53c73f7e947d0dc404b8b0ad3946b3ad8
Nick Stark <nick@nickstark.dev>
2023-07-29 13:56:20 -0500

    add infinite content loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,28 @@ import styles from "@/styles/Home.module.css";
 import { SWAPIPeopleResponse } from "@/types";
 import useSWR from "swr";
 import { CharacterCard } from "@/components/CharacterCard/CharacterCard";
+import useSWRInfinite, { SWRInfiniteKeyLoader } from "swr/infinite";
 
 const getCharacters = (endpoint: string): Promise<SWAPIPeopleResponse> =>
   fetch(endpoint).then((res) => res.json());
 
-export default function Home() {
-  const { data, error, isLoading } = useSWR(
-    "https://swapi.dev/api/people/",
-    getCharacters
-  );
+const getKey: SWRInfiniteKeyLoader = (pageIndex, previousPageData) => {
+  if (previousPageData && !previousPageData.next) return null;
+  console.log("prev page data", previousPageData, pageIndex);
+  return previousPageData
+    ? previousPageData.next
+    : "https://swapi.dev/api/people/";
+};
 
-  //TODO: loading state
-  //TODO: error state
+export default function Home() {
+  const { data, error, isLoading, isValidating, size, setSize } =
+    useSWRInfinite(getKey, getCharacters, { revalidateFirstPage: false });
+  // TODO: add prefetching of next page or infinite scrolling
+
+  const hasMoreToLoad =
+    !data || (data.length > 0 && data[data.length - 1].next !== null);
+  const isLoadingMore =
+    isLoading || (size > 0 && data && typeof data[size - 1] === "undefined");
 
   return (
     <>
@@ -26,18 +36,40 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="preload"
+          href="https://swapi.dev/api/people/"
+          as="fetch"
+          crossOrigin="anonymous"
+        ></link>
       </Head>
       <h1 className={styles.title}>SWAPI Cards</h1>
       <main className={styles.main}>
         <ul className={styles.characterGrid}>
           {data &&
-            data.results.map((character) => (
-              <li key={character.url}>
-                <CharacterCard person={character} />
-              </li>
-            ))}
+            !error &&
+            data.map((dataPage) => {
+              return dataPage.results.map((character) => (
+                <li key={character.url}>
+                  <CharacterCard person={character} />
+                </li>
+              ));
+            })}
         </ul>
-        {isLoading && <div>Loading...</div>}
+        {isLoadingMore && <div className={styles.loading}>Loading...</div>}
+        {hasMoreToLoad && !error && (
+          <div className={styles.btnBar}>
+            <button
+              type="button"
+              disabled={isLoadingMore} // TODO: do this is a more accessibly way
+              className={styles.btn}
+              onClick={() => setSize(size + 1)}
+            >
+              Load more characters
+            </button>
+          </div>
+        )}
+        {error && <div>There was an error loading SWAPI data.</div>}
       </main>
     </>
   );
